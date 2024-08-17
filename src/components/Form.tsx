2386ea8a87d9fb0c715ad1b2fb45f6f90976741c
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-08-17 23:28:33 +0530

    modify form with extra data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,25 +1,28 @@
+'use client'
+
 import React, { useState } from "react";
 import Input from "./Input";
+import { CgArrowLeftR } from "react-icons/cg";
 import api from "../../api";
 
 // import { useNavigate } from "react-router-dom";
 
 const Form = (): JSX.Element => {
   // const navigate = useNavigate();
-  const [FullName, setFullName] = useState<string>("");
+  const [fullName, setFullName] = useState<string>("");
   const [validEmail, setValidEmail] = useState<string>("");
   const [validPassword, setValidPassword] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  const [industry, setIndustry] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [isRegister, setIsRegister] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
-  const [location, setLocation] = useState("")
+  const [location, setLocation] = useState("");
+  const [useAs, setUseAs] = useState("")
 
   const checkEmailPasswordValidation = async (): Promise<void> => {
     setError(""); // Clear previous errors
-
-    console.log("email value: ", email)
     // Validate Email
     const isValidEmail = (): boolean => {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -33,7 +36,8 @@ const Form = (): JSX.Element => {
 
     // Validate Password
     const isValidPassword = (): boolean => {
-      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
       if (passwordRegex.test(password)) {
         return true;
       } else {
@@ -47,7 +51,9 @@ const Form = (): JSX.Element => {
     // Check email and password validity
     if (isValidEmail() && isValidPassword()) {
       try {
-        const response = await api.get(`/api/auth/email-validation?email=${email}`);
+        const response = await api.get(
+          `/api/auth/email-validation?email=${email}`
+        );
         if (response.status === 200) {
           setIsRegister(true);
           setError("Email is already registered");
@@ -61,6 +67,30 @@ const Form = (): JSX.Element => {
         setError("Error validating email. Please try again.");
       }
     }
+  };
+
+  const handleSubmit = async () => {
+    setError("");
+
+    if(!location) {setError("Location is required"); return;}
+    if(!industry) {setError("Industry is required"); return;}
+    if(!useAs) {setError("Use As is required"); return;}
+    
+    
+    if(location && industry && useAs){
+      const formData = new FormData();
+      formData.append("fullName", fullName);
+      formData.append("email", validEmail);
+      formData.append("password", validPassword);
+      formData.append("location", location);
+      formData.append("industry", industry);
+      formData.append("useAs", useAs);
+      const response = await api.post("/api/auth/sign-up")
+
+    }
+
+
+    
   };
 
   return (
@@ -87,15 +117,15 @@ const Form = (): JSX.Element => {
             label="Name"
             id="name"
             type="text"
-            placeholder="Name"
-            value={FullName}
+            placeholder="eg. Jhon duo"
+            value={fullName}
             onChange={(e: any) => setFullName(e.target.value)}
           />
 
           <Input
-            // label="Email"
+            label="Email"
             type="email"
-            placeholder="Email"
+            placeholder="eg. jhonduo@gmail.com"
             value={email}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               setEmail(e.target.value);
@@ -105,10 +135,70 @@ const Form = (): JSX.Element => {
           <Input
             label="Password"
             type="password"
-            placeholder="Password"
+            placeholder="8+ Password"
             value={password}
             onChange={(e: any) => setPassword(e.target.value)}
           />
+
+          <div className="flex justify-end mt-7">
+            <button
+              className="bg-blue-500 mb-6 hover:bg-blue-700 text-white font-semibold py-3 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+              // onClick={checkEmailPasswordValidation}
+              onClick={() => setCurrentIndex(currentIndex + 1)}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+        <div className="w-full flex-shrink-0 p-4">
+          <CgArrowLeftR
+            className="text-black mb-6 text-2xl"
+            onClick={() => setCurrentIndex(currentIndex - 1)}
+          />
+
+          <div className="ml-6 mt-3">
+            {error && (
+              <ul>
+                <li className="text-red-600 font-semibold">{error}</li>
+              </ul>
+            )}
+          </div>
+          <label
+            htmlFor="useFor"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Use As <span className="text-red-700">*</span>
+          </label>
+          <select
+            name=""
+            id="useFor"
+            className="shadow-xl border rounded-xl w-[95%] h-11 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            onChange={(e:any) => setUseAs(e.target.value) }
+          >
+            <option value="0">Select</option>
+            <option value="1">Company</option>
+            <option value="2">Student</option>
+          </select>
+          <div className=" w-full mt-6">
+            <Input
+              label="Industry Name"
+              type="text"
+              value={industry}
+              onChange={(e: any) => setIndustry(e.target.value)}
+              placeholder="eg. IT"
+              className=""
+            />
+          </div>
+          <div className=" w-full mt-2">
+            <Input
+              label="Location"
+              type="text"
+              value={location}
+              onChange={(e: any) => setLocation(e.target.value)}
+              placeholder="eg. Banglore"
+              className=""
+            />
+          </div>
           <div className="relative xl:w-[26rem]">
             <input
               type="checkbox"
@@ -126,39 +216,11 @@ const Form = (): JSX.Element => {
               </a>
             </h2>
           </div>
-
           <div className="flex justify-end mt-7">
-            <button
-              className="bg-blue-500 mb-6 hover:bg-blue-700 text-white font-semibold py-3 px-4 rounded-lg focus:outline-none focus:shadow-outline"
-              onClick={checkEmailPasswordValidation}
-            >
-              Next
+            <button className="bg-blue-500 mb-6 hover:bg-blue-700 text-white font-semibold py-3 px-4 rounded-lg focus:outline-none focus:shadow-outline">
+              Create
             </button>
           </div>
-        </div>
-        <div className="w-full flex-shrink-0 p-4">
-        <div className=' w-full mt-2'>
-                <div className='text-[22px] font-bold'>
-                  Add your location 
-                </div>
-                <Input
-                label="Location"
-                type="text"
-                value={location}
-                onChange={(e:any) => setLocation(e.target.value)}
-                placeholder='Enter a location' className='' />
-            </div>
-          <label htmlFor="useFor"
-          className="block text-gray-700 font-bold mb-2"
-          >Use For</label>
-            <select name="" id="useFor" 
-            className="shadow-xl border rounded-xl w-[95%] h-11 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ">
-              <option value="0">Select</option>
-              <option value="1">Company</option>
-              <option value="2">Student</option>
-              <option value="3"></option>
-              <option value="4">Other</option>
-            </select>
         </div>
       </div>
     </div>
