c90f55c9c642fd69be8c82e0167885ba8d3a2ed2
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-08-21 00:21:43 +0530

    add some stuffs

---
+++ src/app/dashboard/team/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='h-screen w-full border-2 border-neutral-500 rounded-3xl'>
+      team page
+    </div>
+  )
+}
+
+export default page
